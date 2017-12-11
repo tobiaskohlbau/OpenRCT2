29ff999ace7d1f54c96593febf2f358f76b35d9a
Ted John <ted@brambles.org>
2017-12-11 22:01:56 +0000

    Add JSON loading for footpath banners

--- src/openrct2/object/SceneryObject.h
+++ src/openrct2/object/SceneryObject.h
@@ -18,6 +18,7 @@
 
 #ifdef __cplusplus
 
+#include <cstring>
 #include "Object.h"
 
 class SceneryObject : public Object
@@ -33,6 +34,15 @@ public:
 
 protected:
     void SetPrimarySceneryGroup(const rct_object_entry * entry) { _primarySceneryGroupEntry = *entry; }
+    void SetPrimarySceneryGroup(const std::string &s)
+    {
+        if (!s.empty())
+        {
+            rct_object_entry sgEntry = { 0 };
+            std::strncpy(sgEntry.name, s.c_str(), 8);
+            SetPrimarySceneryGroup(&sgEntry);
+        }
+    }
 };
 
 #endif
